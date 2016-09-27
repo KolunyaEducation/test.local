a2c923dc523980b5b6b2bb81f7e1b5fed3bc0683
Nikolai Olisov <wmatrosw@gmail.com>
2016-09-27 08:59:06 +0300

    sort

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
-$a = "hello world, ";
-echo $a . $_GET["name"];
+#?a=15&b=10&c=9&d=9&e=4&f=12&g=7&h=19&i=3&j=17
+$arr = array($_GET["a"],$_GET["b"],$_GET["c"],$_GET["d"],$_GET["e"],$_GET["f"],$_GET["g"],$_GET["h"],$_GET["i"],$_GET["j"]);
+/*
+$array[0] = $_GET["a"];
+$array[1] = $_GET["b"];
+$array[2] = $_GET["c"];
+$array[3] = $_GET["d"];
+$array[4] = $_GET["e"];
+$array[5] = $_GET["f"];
+$array[6] = $_GET["g"];
+$array[7] = $_GET["h"];
+$array[8] = $_GET["i"];
+$array[9] = $_GET["j"];
+
+
+#$a = "hello world, ";
+#echo $a . $_GET["name"];
+#echo $arr[0];
+
+*/
+
+
+
+sort($arr);
+/*
+for ($w=0; $w<=9; $w++) {
+	echo $arr[$w]."<br />";
+	
+}
+*/
+
+print_r($arr);
