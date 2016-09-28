316c38b985264d0cd94241ace86e88cc19a909ed
Nikolai Olisov <wmatrosw@gmail.com>
2016-09-28 12:56:08 +0300

    bubble sort& sort

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-#?a=15&b=10&c=9&d=9&e=4&f=12&g=7&h=19&i=3&j=17
+#?a=1&b=10&c=2&d=21&e=3&f=40&g=5&h=51&i=6&j=67
 $arr = array($_GET["a"],$_GET["b"],$_GET["c"],$_GET["d"],$_GET["e"],$_GET["f"],$_GET["g"],$_GET["h"],$_GET["i"],$_GET["j"]);
+$arr1 = $arr;
 
-#sort($arr);
 
-#print_r($arr);
+
 $s = count($arr)-1;
 for($i=$s; $i>=0; $i--) {
     for($j=0; $j<=($i-1); $j++)
@@ -16,3 +16,6 @@ for($i=$s; $i>=0; $i--) {
 }
 
 print_r($arr);
+echo "\n";
+sort($arr1);
+print_r($arr1);
