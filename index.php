77f68723ac5efc6ced78393e20c2b1e716975e53
Олисов Н. Г <OlisovNG@shuvoe.rg-rus.ru>
2016-10-03 16:38:19 +0300

    sort <>

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ foreach ($a as $value) {
     }
 
 }
+# сортировка по увеличению значения
 
 $s = count($arr)-1;
 for($i=$s; $i>=0; $i--) {
@@ -22,5 +23,24 @@ for($i=$s; $i>=0; $i--) {
 print_r($arr);
 echo '<br/>';
 sort($arr);
+print_r($arr);
+echo '<br/>';
+
+
+# сортировка по уменьшению значения
+
+rsort($arr);
+print_r($arr);
+echo '<br/>';
+
+for($i=$s; $i>=0; $i--) {
+    for($j=0; $j<=($i-1); $j++)
+        if ($arr[$j]<$arr[$j+1]) {
+            $k = $arr[$j];
+            $arr[$j]=$arr[$j+1];
+            $arr[$j+1] = $k;
+        }
+}
+
 print_r($arr);
 echo '<br/>';
