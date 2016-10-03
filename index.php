549e6c331d5d59fb228573c53cfca9204a270a08
Олисов Н. Г <OlisovNG@shuvoe.rg-rus.ru>
2016-10-03 15:54:57 +0300

    use foreach

--- index.php
+++ index.php
@@ -1,34 +1,19 @@
 <?php
-#?a=1&b=10&c=2&d=21&e=3&f=40&g=5&h=51&i=6&j=67
-#($_GET["a"],$_GET["b"],$_GET["c"],$_GET["d"],$_GET["e"],$_GET["f"],$_GET["g"],$_GET["h"],$_GET["i"],$_GET["j"]);
+$a = $_GET['a'];
 
-$arr=array();
+$arr = array();
+foreach ($a as $value) {
+    if(isset($value) && is_numeric($value)) {
+        $arr[]=$value;}
 
-if(isset($_GET['a']) && is_numeric($_GET['a'])) {
-    $arr[]=$_GET['a'];}
-if(isset($_GET['b']) && is_numeric($_GET['b'])) {
-    $arr[]=$_GET['b'];}
-if(isset($_GET['c']) && is_numeric($_GET['c'])){
-    $arr[]=$_GET['c'];}
-if(isset($_GET['d']) && is_numeric($_GET['d'])){
-    $arr[]=$_GET['d'];}
-if(isset($_GET['e']) && is_numeric($_GET['e'])){
-    $arr[]=$_GET['e'];}
-if(isset($_GET['f']) && is_numeric($_GET['f'])){
-    $arr[]=$_GET['f'];}
-if(isset($_GET['g']) && is_numeric($_GET['g'])){
-    $arr[]=$_GET['g'];}
-if(isset($_GET['h']) && is_numeric($_GET['h'])){
-    $arr[]=$_GET['h'];}
-if(isset($_GET['i']) && is_numeric($_GET['i'])){
-    $arr[]=$_GET['i'];}
-if(isset($_GET['j']) && is_numeric($_GET['j'])){
-    $arr[]=$_GET['j'];}
+}
+
+print_r( $arr);
+echo '<br/>';
 
 
 
-$arr1 = $arr;
-
+/*
 
 
 $s = count($arr)-1;
@@ -46,3 +31,4 @@ echo '<br/>';
 sort($arr1);
 print_r($arr1);
 echo '<br/>';
+*/
