5078a85409c893b14fc718f5b88d61589dacf2c4
Пользователь <>
2016-10-07 19:20:49 +0300

    обратная форма правильное оформление

--- index.php
+++ index.php
@@ -1,2 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8'>
+    <title>название странички</title>
+</head>
+<body>
+
+
+<form action='index.php' method='post'>
+    <center><p>Ваше имя: </p></center>
+        <center><p><input type='text' name='name' /></p></center>
+            <center><p>E-mail: </p></center>
+                <center><p><input type='text' name='email' /></p></center>
+                    <center><p>Сообщение:</p></center>
+                        <center><p><textarea rows='10' cols='45' name='message' minlength='5'></textarea></p></center>
+                            <center><input type='submit' name='submit' value='Отправить'></center>
+
+</form>
+
 <?php
-echo "Hello world";
+if (isset($_POST['submit'])) {
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])){
+        if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message'])){
+            echo '<center>Вы не заполнили одно из полей!</center>';
+        }
+        else {
+            if (!preg_match('|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is', strtolower($_POST['email']))) {
+                echo '<center>Вы указали неверно e-mail!</center>';
+            }
+            else {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $msg = $_POST['message'];
+                mail('wmatrosw@gmail.com', 'Сообщение от '.$name, $msg);
+                echo '<center>E-mail отправлен!</center>';
+                echo '<center>$name</center>';
+                echo '<center>$email</center>';
+                echo '<center>$msg</center>';
+            }
+        }
+    }
+}
+?>
+
+</body>
+
+</html>
