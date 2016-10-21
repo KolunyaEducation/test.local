ddcfed0a17a0098a17c97fcc939ef00074cd12e4
Олисов Н. Г <OlisovNG@shuvoe.rg-rus.ru>
2016-10-21 11:27:58 +0300

    rew

--- index.php
+++ index.php
@@ -1,44 +1,19 @@
 <?php
 
-$t=file_get_contents('layout.php');
-echo $t;
-
-
-
 switch ($_GET['page']) {
     case 'main':
-        require __DIR__.'\main.php';
+        $content= require __DIR__.'\main.php';
         break;
     case 'contact':
-        require __DIR__.'\contact.php';
+        $content= require __DIR__.'\contact.php';
         break;
     case 'company':
-        require __DIR__.'\company.php';
+        $content= require __DIR__.'\company.php';
         break;
     default:
         $content= require __DIR__.'\main.php';
         break;
 }
-
-/*
-
-
-include 'main.php';
-
-
-if (isset($_get['page'])) {
-    switch ($_GET['page']) {
-        case 'main':
-            require 'main.php';
-            break;
-        case 'contact':
-            require 'contact.php';
-            break;
-        case 'company':
-            require 'company.php';
-            break;
-    }
-}
-*/
+require_once(__DIR__.'\layout.php');
 
 ?>
