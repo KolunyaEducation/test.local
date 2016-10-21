dd753a009dc99fd138c291a4ebf28c2cf085ebc5
Олисов Н. Г <OlisovNG@shuvoe.rg-rus.ru>
2016-10-21 10:49:08 +0300

    так

--- contact.php
+++ contact.php
@@ -1,58 +1,5 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
-<head>
-    <meta charset="utf-8">
-    <title>Kolunya Education</title>
-    <style>
-        body {
-            font: 11pt Arial, Helvetica, sans-serif; /* Рубленый шрифт текста */
-        }
-        h1 {
-            font-size: 36px; /* Размер шрифта */
-            margin: 0; /* Убираем отступы */
-            color: #fc6; /* Цвет текста */
-        }
-        h2 {
-            margin-top: 0; /* Убираем отступ сверху */
-        }
-        #container {
-            width: 800px; /* Ширина слоя */
-            margin: 0 auto; /* Выравнивание по центру */
 
-        }
-        #header { /* Верхний блок */
-            background: #0080c0; /* Цвет фона */
-            padding: 10px; /* Поля вокруг текста */
-        }
-        #sidebar { /* Левая колонка */
-            float: left; /* Обтекание справа */
-            border: 1px solid #333; /* Параметры рамки вокруг */
-            width: 20%; /* Ширина колонки */
-            padding: 5px; /* Поля вокруг текста */
-            margin: 10px 10px 20px 5px; /* Значения отступов */
-        }
-        #content { /* Правая колонка */
-            margin: 10px 5px 20px 25%; /* Значения отступов */
-            padding: 5px; /* Поля вокруг текста */
-            border: 1px solid #333; /* Параметры рамки */
-        }
-        #footer { /* Нижний блок */
-            background: #333; /* Цвет фона */
-            padding: 5px; /* Поля вокруг текста */
-            color: #fff; /* Цвет текста */
-            clear: left; /* Отменяем действие float */
-        }
-    </style>
-</head>
-<body>
-<div id="container">
-    <div id="header"><h1>Kolunya Education Company</h1></div>
-    <div id="sidebar">
-        <p><a href="index.php">Главная</a></p>
-        <p><a href="company.php">О Компании</a></p>
-        <p><a href="contact.php">Контакты</a></p>
-    </div>
-    <div id="content">
+
         <form action='contact.php' method='post'>
             <center><p>Ваше имя: </p></center>
             <center><p><input type='text' name='name' /></p></center>
@@ -65,9 +12,8 @@
         </form>
 
 
-    </div>
-    <div id="footer">&copy; Kolunya Education</div>
-</div>
+
+
 
 
 <?php
@@ -92,5 +38,3 @@ if (isset($_POST['submit'])) {
 }
 ?>
 
-</body>
-</html>
--- index.php
+++ index.php
@@ -1,62 +1,44 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
-<head>
-    <meta charset="utf-8">
-    <title>Kolunya Education</title>
-    <style>
-        body {
-            font: 11pt Arial, Helvetica, sans-serif; /* Рубленый шрифт текста */
-        }
-        h1 {
-            font-size: 36px; /* Размер шрифта */
-            margin: 0; /* Убираем отступы */
-            color: #fc6; /* Цвет текста */
-        }
-        h2 {
-            margin-top: 0; /* Убираем отступ сверху */
-        }
-        #container {
-            width: 800px; /* Ширина слоя */
-            margin: 0 auto; /* Выравнивание по центру */
+<?php
 
-        }
-        #header { /* Верхний блок */
-            background: #0080c0; /* Цвет фона */
-            padding: 10px; /* Поля вокруг текста */
-        }
-        #sidebar { /* Левая колонка */
-            float: left; /* Обтекание справа */
-            border: 1px solid #333; /* Параметры рамки вокруг */
-            width: 20%; /* Ширина колонки */
-            padding: 5px; /* Поля вокруг текста */
-            margin: 10px 10px 20px 5px; /* Значения отступов */
-        }
-        #content { /* Правая колонка */
-            margin: 10px 5px 20px 25%; /* Значения отступов */
-            padding: 5px; /* Поля вокруг текста */
-            border: 1px solid #333; /* Параметры рамки */
-        }
-        #footer { /* Нижний блок */
-            background: #333; /* Цвет фона */
-            padding: 5px; /* Поля вокруг текста */
-            color: #fff; /* Цвет текста */
-            clear: left; /* Отменяем действие float */
-        }
-    </style>
-</head>
-<body>
-<div id="container">
-    <div id="header"><h1>Kolunya Education Company</h1></div>
-    <div id="sidebar">
-        <p><a href="index.php">Главная</a></p>
-        <p><a href="company.php">О Компании</a></p>
-        <p><a href="contact.php">Контакты</a></p>
-    </div>
-    <div id="content">
-        <h2>Компания обучения Колюни</h2>
-        <p>Здесь вы прочитаете о компании обучения Колюни</p>
-    </div>
-    <div id="footer">&copy; Kolunya Education</div>
-</div>
-</body>
-</html>
+$t=file_get_contents('layout.php');
+echo $t;
+
+
+
+switch ($_GET['page']) {
+    case 'main':
+        require __DIR__.'\main.php';
+        break;
+    case 'contact':
+        require __DIR__.'\contact.php';
+        break;
+    case 'company':
+        require __DIR__.'\company.php';
+        break;
+    default:
+        $content= require __DIR__.'\main.php';
+        break;
+}
+
+/*
+
+
+include 'main.php';
+
+
+if (isset($_get['page'])) {
+    switch ($_GET['page']) {
+        case 'main':
+            require 'main.php';
+            break;
+        case 'contact':
+            require 'contact.php';
+            break;
+        case 'company':
+            require 'company.php';
+            break;
+    }
+}
+*/
+
+?>
